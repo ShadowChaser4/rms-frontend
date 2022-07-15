d6d91bb11f992e24d64d83b92c4741e803f72a82
ShadowChaser4 <kushalacharya48v@outlook.com>
2022-07-15 16:13:03 +0545

    for checking

--- src/Components/Modules/Home/Report/Chart.jsx
+++ src/Components/Modules/Home/Report/Chart.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import {Pie} from 'react-chartjs-2'
+import {Doughnut} from 'react-chartjs-2'
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -28,7 +28,7 @@ function Chart () {
       
   return (
     <>
-        <Pie data={data}/>
+        <Doughnut data={data}/>
     </>
   )
 }
